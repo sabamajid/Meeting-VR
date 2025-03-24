20aa28b8165a955328b1b5bfadf9a7d0867e1442
Thehexatown-Ammar-Naeem <muhammad.ammar@thehexatown.com>
2025-03-24 10:33:23 +0500

    --

---
+++ Assets/SelectedButtonItem.cs
@@ -0,0 +1,13 @@
+using Meta.WitAi;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectedButtonItem : MonoBehaviour
+{
+    public void DeleteSelf()
+    {
+        Destroy(gameObject);
+    }
+
+}
